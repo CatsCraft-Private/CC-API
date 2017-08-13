6c6ea79f9c18ce8ffd7b390c1f62a3232e5814b1
CatsCraft-Private <cbbdevs@gmail.com>
2017-08-13 00:35:40 -0400

    Code to try and fix OP items

--- src/api/brainsynder/Listeners/CreatureSpawnListener.java
+++ src/api/brainsynder/Listeners/CreatureSpawnListener.java
@@ -1,15 +1,24 @@
 package api.brainsynder.Listeners;
 
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Ocelot;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CreatureSpawnListener implements Listener {
-	
-	
+
+
 	@EventHandler (priority = EventPriority.HIGHEST)
 	public void onSpawn(CreatureSpawnEvent e) {
 		if (e.getEntity() instanceof Ocelot) {
@@ -17,5 +26,53 @@ public class CreatureSpawnListener implements Listener {
 				e.setCancelled(false);
 			}
 		}
+	}
+
+	@EventHandler
+	public void onOpen (InventoryOpenEvent event) {
+		ItemStack[] newItems = new ItemStack[event.getInventory().getContents().length];
+		List<ItemStack> items = Arrays.asList(event.getInventory().getContents());
+		int i = 0;
+		for (ItemStack item : items) {
+			if (item == null) {
+				newItems[i] = new ItemStack(Material.AIR);
+				i++;
+				continue;
+			}
+			if (item.getType() == Material.AIR) {
+				newItems[i] = new ItemStack(Material.AIR);
+				i++;
+				continue;
+			}
+
+			net.minecraft.server.v1_8_R3.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+			NBTTagCompound compound = nmsStack.getTag();
+			if (compound == null) {
+				compound = new NBTTagCompound();
+				nmsStack.setTag(compound);
+				compound = nmsStack.getTag();
+			}
+            if (compound.hasKey("AttributeModifiers"))
+                compound.remove("AttributeModifiers");
+			nmsStack.setTag(compound);
+			ItemStack stack = CraftItemStack.asBukkitCopy(nmsStack);
+
+			// Handles Enchantment levels
+
+			for (Enchantment enchant : stack.getEnchantments().keySet()) {
+			    int lvl = stack.getEnchantments().get(enchant);
+			    if (lvl > 6) {
+			        lvl = 5;
+                }
+                stack.removeEnchantment(enchant);
+
+                stack.addUnsafeEnchantment(enchant, lvl);
+            }
+
+			newItems[i] = stack;
+
+			i++;
+		}
+		event.getInventory().setContents(newItems);
 	}
 }
